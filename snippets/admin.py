02053131d990baea81ad6e18d92e84c6f44dbfc4
zainahammouri <hammourizaina78@gmail.com>
2024-03-01 10:48:34 +0300

    add new models

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import  Employees,diabetes, clothes
 
 # Register your models here.
+admin.site.register(Employees)
+admin.site.register(diabetes)
+admin.site.register(clothes)
--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Employees(models.Model):
+    name=models.CharField(max_length=100)
+    checkIn=models.DateTimeField(auto_now_add=True)
+    checkOut=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return self.name
+    
+
+class diabetes(models.Model):
+    glocose=models.FloatField(max_length=10)
+    bmi=models.FloatField(max_length=10)
+    prusser=models.FloatField(max_length=10)
+
+    def __str__(self) :
+        return self.glocose
+    
+class clothes(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.FloatField(max_length=10)
+    descreption=models.TextField()
+
+    def __str__(self) :
+        return self.name
